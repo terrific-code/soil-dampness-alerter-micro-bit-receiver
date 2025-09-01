c7f85092880eb573a3ac4cc8363dd7d74ea17de0
terrific-code <terrific@patternseek.net>
2025-09-01 15:47:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
 radio.onReceivedValue(function (name, value) {
+    music.playTone(262, music.beat(BeatFraction.Whole))
     // or this??
     if (name == "alert" && value == 1) {
-        music.play(music.stringPlayable("C5 C C5 A G C5 C G ", 120), music.PlaybackMode.UntilDone)
+        music.playTone(880, music.beat(BeatFraction.Whole))
     }
 })
+radio.setGroup(2)
+music.playTone(262, music.beat(BeatFraction.Whole))
 let min_soil_moisture = 251
+basic.showLeds(`
+    . . . . .
+    . . . . #
+    . . . # .
+    # . # . .
+    . # . . .
+    `)
