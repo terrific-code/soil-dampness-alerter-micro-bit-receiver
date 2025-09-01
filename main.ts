d529b06b327a8d358bed38facbda66ea584bd611
terrific-code <terrific@patternseek.net>
2025-09-01 13:03:12 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
+radio.onReceivedValue(function (name, value) {
+    // or this??
+    if (name == "alert" && value == 1) {
+        music.play(music.stringPlayable("C5 C C5 A G C5 C G ", 120), music.PlaybackMode.UntilDone)
+    }
 })
+let min_soil_moisture = 251
